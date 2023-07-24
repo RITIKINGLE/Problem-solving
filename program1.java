c9a7b3fbb4824d695ef9197ef4f328674a76ceea
RITIKINGLE <78408525+RITIKINGLE@users.noreply.github.com>
2023-07-24 21:49:31 +0530

    Add files via upload
    swap two values

---
+++ program1.java
@@ -0,0 +1,10 @@
+public class program1{
+	public static void main(String args[]){
+		int a=10, b=20;
+		int c = a;
+		a = b;
+		b = c;
+		System.out.println("a=" +a);
+		System.out.println("b=" +b);
+	}
+}
