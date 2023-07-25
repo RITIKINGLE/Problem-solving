64522f452231b251d697c3883dd87ca07aa049b3
RITIKINGLE <78408525+RITIKINGLE@users.noreply.github.com>
2023-07-25 11:13:01 +0530

    Add files via upload

---
+++ rectangle_block.java
@@ -0,0 +1,20 @@
+public class rectangle_block{
+	
+	public static void main(String args[]){
+		
+		for(int y=1; y<=5; y++){
+	  		for(int i=1; i<=5; i++){
+				for(int j=1; j<=5; j++){
+                  System.out.print("***** ");	
+            }
+                  System.out.println();
+                  
+    	    }
+            System.out.println();
+            
+    
+   }	
+
+ }
+
+}
