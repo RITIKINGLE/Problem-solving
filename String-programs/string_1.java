0de9231b1d3e5abe70cb5832c7a9dad64b7de48b
Ritik Ingle <78408525+RITIKINGLE@users.noreply.github.com>
2023-09-18 21:42:15 +0530

    L-1

---
+++ String-programs/string_1.java
@@ -0,0 +1,13 @@
+//accessing first and last character from the provided string.
+
+public class string_1{
+	public static void main(String args[]){
+		String s1 = "Welcome to my github repository";
+		
+		System.out.println(s1.charAt(0));
+		int length = s1.length();
+
+		System.out.println(s1.charAt(length-1));
+		
+	}
+}
