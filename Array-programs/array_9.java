6df5b8405c96ce4454e9077c2f7e7bef57500244
Ritik Ingle <78408525+RITIKINGLE@users.noreply.github.com>
2023-10-10 22:05:37 +0530

    L-2

---
+++ Array-programs/array_9.java
@@ -0,0 +1,37 @@
+public class array_9{
+	public static void main(String args[]){
+	
+		
+	int a[] ={121,31,144,523,572,354,876,742,192,432,4567,873};
+	int b[] = new int[a.length];
+		int temp=0;
+		for(int i=0; i<a.length; i++)
+		{
+			int count=0, sum=0;
+			int digit1 = a[i]/100;
+			int digit2 = (a[i]/10) % 10;
+			int digit3 = a[i]%10;
+	
+			temp = a[i];
+			while(temp != 0){
+				sum*=10;
+				sum += temp%10;
+				temp/=10;
+				count++;
+			} 
+			
+		
+				for(int j=1; j<count; j++)
+					digit2*=10;
+		
+				b[i] = digit2 + digit3 * 10 + digit1;
+			
+		} 
+		for(int i=0; i<b.length; i++)
+			System.out.print(a[i]+" ");
+		System.out.println();
+		for(int i=0; i<b.length; i++)
+			System.out.print(b[i]+" ");
+	}
+
+}
